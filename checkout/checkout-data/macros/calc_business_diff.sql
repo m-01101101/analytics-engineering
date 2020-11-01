2283e911488c512c0dde92d8ae9cde7445bcc0a4
michael <michaelrogers.arsenal@gmail.com>
2020-11-01 10:31:20 +0000

    updating biz hours macro

--- checkout/checkout-data/macros/calc_business_diff.sql
+++ checkout/checkout-data/macros/calc_business_diff.sql
@@ -1,29 +1,39 @@
-{% macro calc_biz_diff(biz_period, p1, p2) -%}
+{% macro calc_business_diff(biz_period, p1, p2) %}
 
 -- could move these to project vars
 {% set biz_start = 9 %}
 {% set biz_end = 18 %}
 
+{% set start_date_query %}
 WITH start_ref AS (
     SELECT
         CASE 
             WHEN DAYOFWEEK({{ p1 }}) = 6
-            THEN DATEADD(hour, {{ biz_start}}, DATEADD(day, 2, {{ p1 }}))
+            THEN DATEADD(hour, {{ biz_start }}, DATEADD(day, 2, {{ p1 }})::date)
             WHEN DAYOFWEEK({{ p1 }}) = 7
-            THEN DATEADD(hour, {{ biz_start}}, DATEADD(day, 1, {{ p1 }}))
-            WHEN DATE_TRUNC('hour', {{ p1 }}) >= biz_end
+            THEN DATEADD(hour, {{ biz_start }}, DATEADD(day, 1, {{ p1 }})::date)
+            WHEN DATE_PART(hour, {{ p1 }}) >= {{ biz_end }}
             AND DAYOFWEEK({{ p1 }}) = 5
-            THEN DATEADD(hour, {{ biz_start}}, DATEADD(day, 3, {{ p1 }}))
-            WHEN DATE_TRUNC('hour', {{ p1 }}) >= biz_end
-            THEN DATEADD(hour, {{ biz_start}}, DATEADD(day, 1, {{ p1 }}))
+            THEN DATEADD(hour, {{ biz_start }}, DATEADD(day, 3, {{ p1 }})::date)
+            WHEN DATE_PART(hour, {{ p1 }}) >= {{ biz_end }}
+            THEN DATEADD(hour, {{ biz_start }}, DATEADD(day, 1, {{ p1 }})::date)
         END AS ts
 )
 
+SELECT ts::timestamp FROM start_ref
+{% endset %}
+
+{% set results = run_query(start_date_query) %}
+
+{% if execute %}
+{% set input_date = results.columns[0][0] %}
+{% endif %}
+
 , date_spine AS (
     {{ dbt_utils.date_spine(
-        datepart={{biz_period}},
-        start_date=SELECT ts::timestamp_utc FROM start_ref,
-        end_date={{p2}}
+        datepart=biz_period,
+        start_date=input_date,
+        end_date=p2
        )
     }}
 )
@@ -34,11 +44,20 @@ WITH start_ref AS (
     WHERE (
         CASE 
             WHEN {{biz_period}} = 'hour'
-            THEN date_{{biz_period}} >= biz_start
-            AND date_{{biz_period}} <= biz_end
+            THEN 
+                CASE 
+                    WHEN date_{{biz_period}} >= biz_start
+                    AND date_{{biz_period}} <= biz_end
+                    AND DAYOFWEEK(date_{{biz_period}}) NOT IN (6, 7)
+                    THEN 1
+                END
             WHEN {{biz_period}} = 'day'
-            THEN date_{{biz_period}} NOT IN (6, 7)
-        END)
+            THEN 
+                CASE 
+                    WHEN DAYOFWEEK(date_{{biz_period}}) NOT IN (6, 7)
+                    THEN 1
+                END
+        END) = 1
 )
 
 SELECT 
@@ -46,4 +65,4 @@ SELECT
         ARRAYAGG(date_{{biz_period}}) WITHIN GROUP (ORDER BY date_{{biz_period}})
     )
 FROM filtered
-{%- endmacro %}
+{% endmacro %}
--- checkout/checkout-data/models/transform/tr__onboarding_applications.sql
+++ checkout/checkout-data/models/transform/tr__onboarding_applications.sql
@@ -23,15 +23,15 @@ WITH applications AS (
         , opportunities.created_at AS opportunity_created_at
         , merchants.merchant_created_at AS application_complete_at
         , DATEDIFF(day, applications.created_at, merchants.merchant_created_at) AS days_to_onboard
-        , calc_biz_diff("day", applications.created_at, merchants.created_at) AS biz_days_to_onboard
+        -- , calc_business_diff(day, applications.created_at, merchants.created_at) AS biz_days_to_onboard
         , ROW_NUMBER() OVER(
             PARTITION BY application_id 
             ORDER BY COALESCE(modified_at, created_at)
             ) AS progress
         , DATEDIFF(hour, applications.created_at, opportunities.created_at) AS application_to_opportunity_hours
-        , calc_biz_diff("hour", applications.created_at, opportunities.created_at) AS application_to_opportunity_biz_hours
+        -- , calc_business_diff(hour, applications.created_at, opportunities.created_at) AS application_to_opportunity_biz_hours
         , DATEDIFF(hour, applications.created_at, applications.modified_at) AS application_created_to_modified_hours
-        , calc_biz_diff("hour", applications.created_at, opportunities.modified_at) AS application_created_to_modified_biz_hours
+        , calc_business_diff(hour, applications.created_at, opportunities.modified_at) AS application_created_to_modified_biz_hours
     FROM applications
     LEFT JOIN opportunities USING(opportunity_id)
     LEFT JOIN merchants USING(merchant_account_id)
